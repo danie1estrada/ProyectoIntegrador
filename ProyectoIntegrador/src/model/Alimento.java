9f813a4d4873bbf17fa001d2b64c269e11916513
cencom <cencom@cencom-13>
2017-11-21 14:51:11 -0600

    creacion clase Alimento.java

---
+++ ProyectoIntegrador/src/model/Alimento.java
@@ -0,0 +1,35 @@
+package model;
+
+public class Alimento extends Producto {
+
+    private String marca;
+    private String fechaCad;
+
+    public Alimento(int codigo, float precio, String descripcion, String marca, String fechaCad) {
+        super(codigo, precio, descripcion);
+        this.marca = marca;
+        this.fechaCad = fechaCad;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getFechaCad() {
+        return fechaCad;
+    }
+
+    public void setFechaCad(String fechaCad) {
+        this.fechaCad = fechaCad;
+    }
+
+    @Override
+    public String[] getInfo() {
+        return new String[]{Integer.toString(codigo), Float.toString(precio),
+            descripcion, marca, fechaCad};
+    }
+}

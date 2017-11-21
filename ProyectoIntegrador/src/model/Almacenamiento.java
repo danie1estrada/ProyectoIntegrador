7c69d6e5df9d52d9cd9e094ff647ea5c14504833
Daniel Estrada <33325631+danie1estrada@users.noreply.github.com>
2017-11-21 15:23:06 -0600

    Update Almacenamiento.java

--- ProyectoIntegrador/src/model/Almacenamiento.java
+++ ProyectoIntegrador/src/model/Almacenamiento.java
@@ -4,9 +4,6 @@ package model;
 public interface Almacenamiento {
     
     public void guardar(Producto p, String tabla);
-    
     public Producto getProducto(int codigo);
-        
-    
     
 }

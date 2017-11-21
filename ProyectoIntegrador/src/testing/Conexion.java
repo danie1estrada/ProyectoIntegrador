6a09a5096d1be041760bf0c9cd1bac8b51408478
cencom <cencom@cencom-12>
2017-11-21 15:10:36 -0600

    Documentacion para las clases Conexion.java y DemoDB.java

--- ProyectoIntegrador/src/testing/Conexion.java
+++ ProyectoIntegrador/src/testing/Conexion.java
@@ -15,17 +15,31 @@ public class Conexion {
     private Connection c;
     private Statement st;
     
+    /**
+     * Crea una conexion con 
+     */
     public Conexion() {
         url = "jdbc:mysql://localhost:3306/prueba";
         user = "root";
         password = "admin";
     }
     
+    /**
+     * Conecta con la base de datos de MySQL 'prueba'
+     * 
+     * @throws SQLException 
+     */
     public void connect() throws SQLException {
         c = DriverManager.getConnection(url, user, password);
         st = c.createStatement();
     }
     
+    /**
+     * @param data Arreglo con los datos a almacenar dentro de la base de datos
+     * @param table Tabla en la base de datos donde se guardarán los registros
+     * 
+     * @throws SQLException 
+     */
     public void insert(String [] data, String table) throws SQLException {
         String query = "INSERT INTO " + table + " VALUES (";
         
@@ -43,8 +57,14 @@ public class Conexion {
         st.execute(query);
     }
     
-    public void select() throws SQLException {
-        ResultSet rs = st.executeQuery("SELECT * FROM Producto");
+    /**
+     * Muestra todos los registros de una tabla especifica en la base de datos.
+     * 
+     * @param table La tabla en la base de datos de donde se consultarán los datos
+     * @throws SQLException 
+     */
+    public void select(String table) throws SQLException {
+        ResultSet rs = st.executeQuery("SELECT * FROM " + table);
         ResultSetMetaData md = rs.getMetaData();
         
         int cols = md.getColumnCount();
@@ -55,6 +75,5 @@ public class Conexion {
             }
             System.out.println();
         }
-        
     }
 }

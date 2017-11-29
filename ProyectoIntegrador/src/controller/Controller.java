0effc6b8bcfec2f1e95c0aaede2aaeb11889c2c6
unknown <tostadoskate@gmail.com>
2017-11-29 15:47:11 -0600

    validacion de "descripcion y precio"

--- ProyectoIntegrador/src/controller/Controller.java
+++ ProyectoIntegrador/src/controller/Controller.java
@@ -10,5 +10,39 @@ package controller;
  * @author cencom
  */
 public class Controller {
-    
+    /* para evaluar si una cadena de texto es un numero*/
+    public boolean Validar(String text){
+        
+        try{
+            
+            Float.parseFloat(text);
+            return true;
+            
+        }catch(Exception e){
+            return false;
+        }
+        
+        
+        
+    }
+    /* para evaluar la longitud de una cadena de String
+    en este caso nos basamos en la descripcion*/
+    public  boolean ValidarString(String text, int max){
+       
+        if(text.length() <= max && !text.isEmpty()){
+            return true;
+        }
+        
+        return false;
+    }
+    /*para evaluar la cantidad monetaria de la venta */
+    public  boolean ValidarPrecio(float precio){
+        
+        if(precio < 10000 && precio > 0){
+            return true;
+        }
+        
+        return false;
+    }
+
 }
